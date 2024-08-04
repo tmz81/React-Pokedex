914170b04abf86e34b10460de1417cd23cef8edf
tmz81 <tmz.zt4@gmail.com>
2024-08-04 18:39:53 -0300

    fix mini bugs

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,10 +10,10 @@ interface Pokemon {
   order: number;
   sprites: {
     other: {
-      'official-artwork': {
+      "official-artwork": {
         front_default: string;
-      }
-    }
+      };
+    };
   };
   types: PokemonType[];
 }
@@ -26,50 +26,81 @@ interface PokemonType {
 }
 
 export function Home() {
-  const [pokedex, setPokedex] = useState<Pokemon[]>([] as Pokemon[]);
+  const [pokedex, setPokedex] = useState<Pokemon[]>([]);
+  const [filteredPokedex, setFilteredPokedex] = useState<Pokemon[]>([]);
 
-  const GetListPokemons = useCallback(async (FirstId: number, LastId: number) => {
-    for (let i = FirstId; i <= LastId; i++) {
-      const pokemonSearch = localStorage.getItem(
-        `pokemon:${i}`,
-      );
+  const GetListPokemons = useCallback(
+    async (FirstId: number, LastId: number) => {
+      const fetchedPokemons: Pokemon[] = [];
 
-      if (pokemonSearch)
-        setPokedex(oldPokedex => [...oldPokedex, JSON.parse(pokemonSearch)]);
-      else {
-        const { data } = await Promise.resolve(api.get(`pokemon/${i}`));
+      for (let i = FirstId; i <= LastId; i++) {
+        const pokemonSearch = localStorage.getItem(`pokemon:${i}`);
 
-        localStorage.setItem(
-          `pokemon:${i}`,
-          JSON.stringify(data),
-        );
-
-        setPokedex(oldPokedex => [...oldPokedex, data]);
+        if (pokemonSearch) {
+          fetchedPokemons.push(JSON.parse(pokemonSearch));
+        } else {
+          const { data } = await api.get(`pokemon/${i}`);
+          localStorage.setItem(`pokemon:${i}`, JSON.stringify(data));
+          fetchedPokemons.push(data);
+        }
       }
-    }
-  }, []);
+
+      setPokedex((oldPokedex) => {
+        const combinedPokedex = [...oldPokedex, ...fetchedPokemons];
+        const uniquePokedex = Array.from(
+          new Set(combinedPokedex.map((p) => p.id))
+        ).map((id) => combinedPokedex.find((p) => p.id === id));
+        setFilteredPokedex(uniquePokedex as Pokemon[]);
+        return uniquePokedex as Pokemon[];
+      });
+    },
+    []
+  );
 
   useEffect(() => {
     GetListPokemons(1, 20);
-  }, [setPokedex]);
+  }, [GetListPokemons]);
+
+  const handleSearch = useCallback(
+    async (query: string) => {
+      if (query === "") {
+        setFilteredPokedex(pokedex);
+      } else {
+        try {
+          const { data } = await api.get(`pokemon/${query.toLowerCase()}`);
+          setFilteredPokedex([data]);
+        } catch (error) {
+          const filtered = pokedex.filter((pokemon) =>
+            pokemon.name.toLowerCase().includes(query.toLowerCase())
+          );
+          setFilteredPokedex(filtered);
+        }
+      }
+    },
+    [pokedex]
+  );
+
+  useEffect(() => {
+    setFilteredPokedex(pokedex); // Atualizar a lista filtrada quando o pokedex for atualizado
+  }, [pokedex]);
 
   return (
     <>
       <Header>
         <Logo />
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
       </Header>
       <section>
         <PokemonList>
-          {pokedex.map(pokemon => (
+          {filteredPokedex.map((pokemon) => (
             <PokemonItem
               key={pokemon.id}
               pokemon={pokemon}
-              sprite={pokemon.sprites.other['official-artwork'].front_default}
+              sprite={pokemon.sprites.other["official-artwork"].front_default}
             />
           ))}
         </PokemonList>
       </section>
     </>
-  )
+  );
 }
